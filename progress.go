15dcf4e8e6d8a4326f458d194b73cd71b8456c1d
whatwewant <tobewhatwewant@outlook.com>
2022-09-05 13:42:25 +0800

    fix: lint error missing docs

--- progress.go
+++ progress.go
@@ -4,12 +4,14 @@ package fetch
 //	https://github.com/schollz/progressbar/blob/master/progressbar.go
 //  https://stackoverflow.com/questions/26050380/go-tracking-post-request-progress
 
+// Progress is a progress event
 type Progress struct {
 	Reporter func(percent int64, current, total int64)
 	Total    int64
 	Current  int64
 }
 
+// Write writes the data to the progress event
 func (p *Progress) Write(b []byte) (n int, err error) {
 	n = len(b)
 	p.Current += int64(n)
