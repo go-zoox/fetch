ae960d1d01b7c560236b6b78d33215855ef5de27
Cedrick AHOUANGANSI <cedrickentrep@gmail.com>
2025-06-16 20:06:54 +0100

    feat: block body when request method is GET or HEAD

--- head.go
+++ head.go
@@ -9,5 +9,9 @@ func Head(url string, config ...interface{}) (*Response, error) {
 		return nil, ErrTooManyArguments
 	}
 
+	if c.Body != nil {
+        panic("Request with HEAD method cannot have body")
+    }
+
 	return New().Head(url, c).Execute()
 }
